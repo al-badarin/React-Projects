591718cbe2bbc3b0ce000b3f48b3bbb059fadba0
al-badarin <jamalalbadarin@gmail.com>
2025-07-29 11:02:14 +0300

    Enable editing player names in Tic-Tac-Toe
    Refactored Player component to use 'initialName' prop and allow editing player names via input field. Updated App.jsx to pass 'initialName' instead of 'name'.

--- 02-Tic-Tac-Toe-game-project/src/components/Player/Player.jsx
+++ 02-Tic-Tac-Toe-game-project/src/components/Player/Player.jsx
@@ -2,18 +2,30 @@ import { useState } from 'react';
 
 import './Player.css';
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing); // this way we are always working with the latest available state
   }
 
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
   return (
     <li>
       <span className="player">
-        {!isEditing && <span className="player-name">{name}</span>}
-        {isEditing && <input type="text" required value={name} />}
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
 

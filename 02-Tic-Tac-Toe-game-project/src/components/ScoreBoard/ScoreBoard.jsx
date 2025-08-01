4a52609dd1fc889730ccc7252cef4dabf5fe7854
al-badarin <jamalalbadarin@gmail.com>
2025-08-01 08:36:02 +0300

    Update ScoreBoard.jsx

--- 02-Tic-Tac-Toe-game-project/src/components/ScoreBoard/ScoreBoard.jsx
+++ 02-Tic-Tac-Toe-game-project/src/components/ScoreBoard/ScoreBoard.jsx
@@ -3,8 +3,12 @@ import './ScoreBoard.css';
 export default function ScoreBoard({ players, scores, onReset }) {
   return (
     <div className="scoreboard">
-      <p>{players.X} Wins: {scores.X}</p>
-      <p>{players.O} Wins: {scores.O}</p>
+      <p>
+        🏆 {players.X} Wins: {scores.X}
+      </p>
+      <p>
+        🏆 {players.O} Wins: {scores.O}
+      </p>
       <button onClick={onReset}>Reset Game & Scores</button>
     </div>
   );

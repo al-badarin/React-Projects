6f9ae9276d2ea31b9972d1208dc44a3f32c06094
al-badarin <jamalalbadarin@gmail.com>
2025-08-02 15:02:33 +0300

    Integrate investment calculation in Results component
    Replaced logging of input props with calculation and logging of investment results using the calculateInvestmentResults utility.

--- 03-Investment-Calculator/src/components/Results/Results.jsx
+++ 03-Investment-Calculator/src/components/Results/Results.jsx
@@ -1,6 +1,8 @@
+import { calculateInvestmentResults } from '../../util/investment';
 import './Results.css';
 
 export default function Results({ input }) {
-  console.log(input);
+  const resultsData = calculateInvestmentResults(input);
+  console.log(resultsData);
   return <p>Results...</p>;
 }

a47fb713e39075f7811d8a0df9f2d73a3fca98b8
al-badarin <jamalalbadarin@gmail.com>
2025-08-02 07:55:34 +0300

    Create Header.jsx

---
+++ 03-Investment-Calculator/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+// import {} from '../../assets' 
+
+export default function Header() {
+  return (
+    <header id="header">
+      <img src="../../assets/investment-calculator-logo.png" alt="" />
+      <h1></h1>
+    </header>
+  );
+}

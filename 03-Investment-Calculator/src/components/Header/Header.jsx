2edf1a9a0a3421ef8999f719178d374f1f124366
al-badarin <jamalalbadarin@gmail.com>
2025-08-02 07:57:28 +0300

    Add Header component and update App to use it
    Introduced a Header component displaying the app title and logo. Updated App.jsx to render the Header component instead of a plain heading.

--- 03-Investment-Calculator/src/components/Header/Header.jsx
+++ 03-Investment-Calculator/src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ import './Header.css';
 export default function Header() {
   return (
     <header id="header">
-      <img src="../../assets/investment-calculator-logo.png" alt="" />
-      <h1></h1>
+      <img src="investment-calculator-logo.png" alt="" />
+      <h1>React Investment Calculator</h1>
     </header>
   );
 }

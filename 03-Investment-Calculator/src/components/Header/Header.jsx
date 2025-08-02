d2e439f055dc7b87a23b057c5f804298779a4b7f
al-badarin <jamalalbadarin@gmail.com>
2025-08-02 12:59:57 +0300

    Update header title text
    Changed the header title from 'React Investment Calculator' to 'Investment Calculator' for a more general label.

--- 03-Investment-Calculator/src/components/Header/Header.jsx
+++ 03-Investment-Calculator/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header id="header">
       <img src={logo} alt="Logo shhowing a money bag" />
-      <h1>React Investment Calculator</h1>
+      <h1>Investment Calculator</h1>
     </header>
   );
 }

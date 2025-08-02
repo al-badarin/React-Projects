05a16c2e88c5e8a95628af291d0e58680f476e2c
al-badarin <jamalalbadarin@gmail.com>
2025-08-02 12:15:39 +0300

    Add UserInput component and integrate into App
    Created a new UserInput component with a basic input field and added it to the App component. This sets up the structure for user input in the investment calculator.

---
+++ 03-Investment-Calculator/src/components/UserInput/UserInput.jsx
@@ -0,0 +1,10 @@
+import './UserInput.css';
+
+export default function UserInput() {
+  return (
+    <section id="user-input">
+      <label></label>
+      <input type="text" />
+    </section>
+  );
+}

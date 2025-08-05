b21c0453f3f4013ffbcf5285d765999232fc59ae
al-badarin <jamalalbadarin@gmail.com>
2025-08-05 16:37:33 +0300

    Create Player.jsx

---
+++ 04-Countdown-Game/src/components/Player/Player.jsx
@@ -0,0 +1,13 @@
+import './Player.css'
+
+export default function Player() {
+  return (
+    <section id="player">
+      <h2>Welcome unknown entity</h2>
+      <p>
+        <input type="text" />
+        <button>Set Name</button>
+      </p>
+    </section>
+  );
+}

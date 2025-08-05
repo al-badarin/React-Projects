3c4d67e5aed59bdb0c9d4f4c06503815a249b022
al-badarin <jamalalbadarin@gmail.com>
2025-08-05 16:42:32 +0300

    Add TimerChallenge component and integrate in App
    Introduced the TimerChallenge component to display individual timer challenges with a title and target time. Updated App.jsx to render multiple TimerChallenge instances with different difficulty levels.

--- 04-Countdown-Game/src/App.jsx
+++ 04-Countdown-Game/src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from './components/Player/Player.jsx';
+import TimerChallenge from './components/TimerChallenge/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime="1" />
+        <TimerChallenge title="Not Easy" targetTime="5" />
+        <TimerChallenge title="Getting Tough" targetTime="10" />
+        <TimerChallenge title="Only for pros" targetTime="15" />
+      </div>
     </>
   );
 }

ea0f08a45ca74a6a9a66afbd5e54d9dfa9620fb0
al-badarin <jamalalbadarin@gmail.com>
2025-08-10 15:49:57 +0300

    Refactor ResultModal to use imperative handle for dialog
    ResultModal now uses useImperativeHandle to expose an open() method, allowing parent components to control the dialog via a ref. TimerChallenge is updated to call dialog.current.open() instead of showModal().

--- 04-Countdown-Game/src/components/ResultModal/ResultModal.jsx
+++ 04-Countdown-Game/src/components/ResultModal/ResultModal.jsx
@@ -1,12 +1,22 @@
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import './ResultModal.css';
 
 const ResultModal = forwardRef(function ResultModal(
   { result, targetTime },
   ref
 ) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showDialog();
+      },
+    };
+  });
+
   return (
-    <dialog ref={ref} className="result-modal">
+    <dialog ref={dialog} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>

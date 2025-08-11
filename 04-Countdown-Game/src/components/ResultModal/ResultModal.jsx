8fe79aaec2aeedf0d142db32a3bf54dcd7914107
al-badarin <jamalalbadarin@gmail.com>
2025-08-11 16:56:05 +0300

    Refactor ResultModal to use createPortal and update props
    ResultModal now uses createPortal to render the dialog into a modal root. The component's props have been updated to accept targetTime, remainingTime, and onReset, and the score calculation and display logic have been improved. The close button now triggers onReset, and the dialog content reflects whether the user lost or their score.

--- 04-Countdown-Game/src/components/ResultModal/ResultModal.jsx
+++ 04-Countdown-Game/src/components/ResultModal/ResultModal.jsx
@@ -1,11 +1,17 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
 import './ResultModal.css';
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -15,19 +21,31 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
 
-  return (
+  return createPortal(
+    //todo:
+    // The <dialog> element allows website visitors to close the opened dialog by pressing the ESC (Escape) key on their keyboard.
+
+    // Currently, this will not trigger the onReset function though (unlike closing the dialog with a button click).
+
+    // To make sure that onReset gets triggered when the dialog is closed via the escape key, you should add the built-in onClose prop to the <dialog> element and bind it to the onReset prop value.*/
+
+    //* <dialog ref={dialog} className="result-modal" onClose={onReset}>
     <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost!</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left</strong>.
+        You stopped the timer with
+        <strong>{formattedRemainingTime} seconds left</strong>.
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   );
 });
 

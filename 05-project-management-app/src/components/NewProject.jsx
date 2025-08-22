8c41894784b50c73b9ebeba0c7a3005317f5f9ab
al-badarin <jamalalbadarin@gmail.com>
2025-08-22 08:24:44 +0300

    Add project creation functionality
    Introduces the ability to add new projects by passing project data from NewProject to App via a callback. The NewProject component now calls onAdd with entered values when the Save button is clicked.

--- 05-project-management-app/src/components/NewProject.jsx
+++ 05-project-management-app/src/components/NewProject.jsx
@@ -1,10 +1,24 @@
 import { useRef } from 'react';
 import Input from './Input';
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    // todo: add validation...
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      enteredDueDate: enteredDueDate,
+    });
+  }
 
   return (
     <div className="w-[35rem] mt-16">
@@ -15,7 +29,10 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            onClick={handleSave}
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+          >
             Save
           </button>
         </li>

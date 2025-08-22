34f548a1c28ffe055e99b2ce252ed223c612f82e
al-badarin <jamalalbadarin@gmail.com>
2025-08-22 10:17:46 +0300

    Add imperative open method to Modal component
    Introduced useImperativeHandle and a ref to allow parent components to programmatically open the Modal by calling the exposed open() method. This enhances the Modal's usability and control from outside the component.

--- 05-project-management-app/src/components/Modal.jsx
+++ 05-project-management-app/src/components/Modal.jsx
@@ -1,9 +1,19 @@
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = forwardRef(function Modal({ children }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return createPortal(
-    <dialog>{children}</dialog>,
+    <dialog ref={dialog}>{children}</dialog>,
     document.getElementById('modal-root')
   );
 });

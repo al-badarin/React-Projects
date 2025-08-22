eab7b638c8ad9dc62f0152cb69298aef9e261c73
al-badarin <jamalalbadarin@gmail.com>
2025-08-22 10:21:11 +0300

    Add validation and modal to NewProject form
    Introduces input validation for the NewProject form, ensuring all fields are filled before submission. Displays a modal dialog when validation fails to inform the user.

--- 05-project-management-app/src/components/NewProject.jsx
+++ 05-project-management-app/src/components/NewProject.jsx
@@ -1,7 +1,11 @@
 import { useRef } from 'react';
+
 import Input from './Input';
+import Modal from './Modal';
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +15,14 @@ export default function NewProject({ onAdd }) {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    // todo: add validation...
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -21,28 +32,32 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSave}
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
+    <>
+      <Modal ref={modal} />
 
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={dueDate} label="Due Date" />
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }

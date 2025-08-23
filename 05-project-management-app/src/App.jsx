3437ec61d6dda4864dee15cd979cf59f9256b65e
al-badarin <jamalalbadarin@gmail.com>
2025-08-23 11:56:42 +0300

    Implement add task functionality in App
    The handleAddTask function now creates a new task with a unique id, project association, and text, then updates the tasks state. This enables adding tasks to the selected project.

--- 05-project-management-app/src/App.jsx
+++ 05-project-management-app/src/App.jsx
@@ -12,8 +12,20 @@ function App() {
     tasks: [],
   });
 
-  function handleAddTask() {
-    //todo
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   function handleDeleteTask() {

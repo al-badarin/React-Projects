0c0abca54e12ca46414a0f8bd8127685bf1c836a
al-badarin <jamalalbadarin@gmail.com>
2025-08-23 12:02:34 +0300

    Render task list with delete button
    Updated the Tasks component to display a styled list of tasks with a 'Clear' button for each task. This improves task visibility and prepares for future task deletion functionality.

--- 05-project-management-app/src/components/Tasks.jsx
+++ 05-project-management-app/src/components/Tasks.jsx
@@ -11,7 +11,18 @@ export default function Tasks({ tasks, onAdd, onDelete }) {
         </p>
       )}
 
-      {tasks.length > 0 && <ul></ul>}
+      {tasks.length > 0 && (
+        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+          {tasks.map((task) => (
+            <li key={task.id} className="flex justify-between my-4">
+              <span>{task.text}</span>
+              <button className="text-stone-700 hover:text-red-500">
+                Clear
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }

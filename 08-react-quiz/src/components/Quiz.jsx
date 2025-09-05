f9d16aea4ce38c00907e3b8bf9d4b8f44b68e6a1
al-badarin <jamalalbadarin@gmail.com>
2025-09-05 18:54:40 +0300

    Implement quiz question rendering and answer selection
    Quiz component now displays the current question and its answers, shuffling the answer order. User selections are tracked and update the active question index.

--- 08-react-quiz/src/components/Quiz.jsx
+++ 08-react-quiz/src/components/Quiz.jsx
@@ -1,8 +1,34 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div>
+      <div id="questions">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }

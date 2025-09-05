bf6a593883d2ca2fade88ac42b360e423859f137
al-badarin <jamalalbadarin@gmail.com>
2025-09-05 19:00:02 +0300

    Add quiz completion summary screen
    Introduces a summary screen with an image and message when the quiz is completed. Refactors logic to check for quiz completion before rendering questions.

--- 08-react-quiz/src/components/Quiz.jsx
+++ 08-react-quiz/src/components/Quiz.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 import QUESTIONS from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -15,8 +15,20 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
-    <div>
+    <div id="quiz">
       <div id="questions">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
